01b858c346d6cf8ee353c2f8e415a455880f4269
David Dalström <david.dalstrom@gmail.com>
2018-12-07 18:11:51 +0100

    adding day4

---
+++ advent/day4/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+    std::ifstream file(argv[1]);
+    std::string line;
+
+    while (std::getline(file, line)) {
+
+    }
+
+    return 0;
+}
