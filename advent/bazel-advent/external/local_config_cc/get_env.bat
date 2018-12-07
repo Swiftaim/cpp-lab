48a111895ebfc75640e690e4df9bd6725a40f775
David Dalström <david.dalstrom@gmail.com>
2018-12-07 17:31:23 +0100

    cleanup

--- advent/bazel-advent/external/local_config_cc/get_env.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\\VCVARSALL.BAT" amd64 > NUL 
-echo PATH=%PATH%,INCLUDE=%INCLUDE%,LIB=%LIB%,WINDOWSSDKDIR=%WINDOWSSDKDIR% 
--- advent/bazel-advent/external/local_config_cc/get_vc_dir.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\\vcvarsqueryregistry.bat"
-echo %VCINSTALLDIR%
