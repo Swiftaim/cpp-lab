e08f835e97de2364274d6a73fe370e646ec2be15
David Dalström <david.dalstrom@gmail.com>
2018-12-08 16:23:50 +0100

    day4 part one

--- advent/day4/main.cpp
+++ advent/day4/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 enum class Event
 {
@@ -21,6 +22,26 @@ struct Record
     std::string date;
     std::string note;
     int guard = -1;
+
+    static bool compare(const Record& l, const Record& r)
+    {
+        if (l.date < r.date)
+            return true;
+        if (l.date > r.date)
+            return false;
+        if (l.time.hour < r.time.hour)
+            return true;
+        if (l.time.hour > r.time.hour)
+            return false;
+        if (l.time.minute < r.time.minute)
+            return true;
+        return false;
+    }
+
+    bool operator==(const Record& r)
+    {
+        return guard == r.guard;
+    }
 };
 
 std::ostream& operator<<(std::ostream& o, const Record& r)
@@ -105,10 +126,63 @@ int main(int argc, char* argv[])
 {
     std::ifstream file(argv[1]);
     std::string line;
-
+    std::vector<Record> records;
     while (std::getline(file, line)) {
-        std::cout << parse(line) << std::endl;
+        records.push_back(parse(line));
+    }
+    std::sort(records.begin(), records.end(), Record::compare);
+    
+    // guards an their sleep times
+    std::unordered_map<int, std::unordered_map<int, int>> asleep;
+    int asleeptime = -1;
+    int guard = -1;
+    for (const auto& r : records)
+    {
+        if (r.guard != -1)
+        {
+            guard = r.guard;
+        } else if (r.note.find("asleep") != std::string::npos)
+        {
+            asleeptime = r.time.minute;
+        } else if (r.note.find("wakes"))
+        {
+            assert(asleeptime > -1);
+            int wokeuptime = r.time.minute;
+            for (int t = asleeptime; t < wokeuptime; ++t)
+                asleep[guard][t] += 1;
+            asleeptime = -1;
+        }
     }
 
+    int maxsleeptime = 0;
+    int selectedguard = -1;
+    for (const auto& guardsleep : asleep)
+    {
+        int minutes = 0;
+        const auto& asleepMap = guardsleep.second;
+        for (const auto& p : asleepMap)
+            minutes += p.second;
+        std::cout << "guard #" << guardsleep.first << " asleep " << minutes << std::endl;
+        if (minutes > maxsleeptime)
+        {
+            maxsleeptime = minutes;
+            selectedguard = guardsleep.first;
+        }
+    }
+    std::cout << "Guard #" << selectedguard << " slept for " << maxsleeptime << " minutes" << std::endl;
+
+    int sleepyminute = -1;
+    int maxrepeats = 0;
+    const auto& guardSleepLog = asleep[selectedguard];
+    for (const auto& p : guardSleepLog)
+    {
+        if (p.second > maxrepeats)
+        {
+            sleepyminute = p.first;
+            maxrepeats = p.second;
+        }
+    }
+
+    std::cout << "Chosen guard: #" << selectedguard << ", chosen minute: " << sleepyminute << "; Answer: " << selectedguard * sleepyminute << std::endl;
     return 0;
 }
